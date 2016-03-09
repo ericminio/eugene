f2e3165702495359b6cc0c35bc1fe68a1e4a4a48
Eric Mignot <eric.mignot@gmail.com>
2016-03-09 16:02:55 -0500

    Eugene can log only selected categories (fixes #2)

--- app/2.only.spec.js
+++ app/2.only.spec.js
@@ -1,17 +1,32 @@
 var expect = require('chai').expect;
 var log = require('./lib/eugene');
 
-describe('Logging only one category', function() {
+describe('Logging', function() {
 
     beforeEach(function() {
         log.output = require('./support/in.memory.output')();
     });
 
-    it('is so easy with Eugene', function() {
-        log.only('this category');
-        log({ category:'this category', message:'will be logged' });
-        log({ category:'this log', message:'will be ignored' });
+    describe('only one category', function() {
 
-        expect(log.output.messages).to.deep.equal(['will be logged']);
+        it('is so easy with Eugene', function() {
+            log.only('this category');
+            log({ category:'this category', message:'will be logged' });
+            log({ category:'this log', message:'will be ignored' });
+
+            expect(log.output.messages).to.deep.equal(['will be logged']);
+        });
+    });
+
+    describe('only two categories', function() {
+
+        it('is also so easy with Eugene', function() {
+            log.only(['first', 'second']);
+            log({ category:'first', message:'first log' });
+            log({ category:'any', message:'will be ignored' });
+            log({ category:'second', message:'second log' });
+
+            expect(log.output.messages).to.deep.equal(['first log', 'second log']);
+        });
     });
 });
--- app/lib/eugene.js
+++ app/lib/eugene.js
@@ -2,13 +2,16 @@ var log = function(options, message) {
     var category = options.category || options;
     var message = message || options.message;
 
-    if (!log.category || category == log.category) {
+    if (log.shouldLog(category)) {
         log.output.write(message);
     }
 };
 
-log.only = function(category) {
-    log.category = category;
+log.only = function(categories) {
+    log.categories= categories;
+};
+log.shouldLog = function(category) {
+    return !log.categories || log.categories.indexOf(category)!= -1
 };
 
 module.exports = log;
