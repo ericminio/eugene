5bbee179ddac52440910cb3411a50b536bfdec9f
Eric Mignot <eric.mignot@gmail.com>
2016-03-11 16:20:50 -0500

    Version is 1.2.0

--- demo/tictac.js
+++ demo/tictac.js
@@ -1,4 +1,4 @@
-var eugene = require('../app/lib/eugene');
+var eugene = require('eugene');
 
 var defaultRenderer = eugene.renderer;
 eugene.renderer = function(category, message) { return message; };
