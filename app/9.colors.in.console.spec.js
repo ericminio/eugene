d8bf32b76ea6b9ca2665dd64e0e7dd7d6a58e861
Stoyan Mishkovski <mishkovski@gmail.com>
2016-09-22 17:31:21 -0400

    Map console colors

--- app/9.colors.in.console.spec.js
+++ app/9.colors.in.console.spec.js
@@ -35,4 +35,38 @@ describe('Color in console', function() {
         expect(console.log).to.have.been.calledWith('Hello console!');
     });
 
+    it('is mapped to the correct code', function() {
+        writer.colors = { 'black category': 'black' };
+        writer.write('Hello console!', 'black category');
+        expect(console.log).to.have.been.calledWith('\x1b[30mHello console!\x1b[0m');
+
+        writer.colors = { 'red category': 'red' };
+        writer.write('Hello console!', 'red category');
+        expect(console.log).to.have.been.calledWith('\x1b[31mHello console!\x1b[0m');
+
+        writer.colors = { 'green category': 'green' };
+        writer.write('Hello console!', 'green category');
+        expect(console.log).to.have.been.calledWith('\x1b[32mHello console!\x1b[0m');
+
+        writer.colors = { 'yellow category': 'yellow' };
+        writer.write('Hello console!', 'yellow category');
+        expect(console.log).to.have.been.calledWith('\x1b[33mHello console!\x1b[0m');
+
+        writer.colors = { 'blue category': 'blue' };
+        writer.write('Hello console!', 'blue category');
+        expect(console.log).to.have.been.calledWith('\x1b[34mHello console!\x1b[0m');
+
+        writer.colors = { 'magneta category': 'magneta' };
+        writer.write('Hello console!', 'magneta category');
+        expect(console.log).to.have.been.calledWith('\x1b[35mHello console!\x1b[0m');
+
+        writer.colors = { 'cyan category': 'cyan' };
+        writer.write('Hello console!', 'cyan category');
+        expect(console.log).to.have.been.calledWith('\x1b[36mHello console!\x1b[0m');
+
+        writer.colors = { 'white category': 'white' };
+        writer.write('Hello console!', 'white category');
+        expect(console.log).to.have.been.calledWith('\x1b[37mHello console!\x1b[0m');  
+    });
+
 });
--- app/lib/console.output.js
+++ app/lib/console.output.js
@@ -1,10 +1,24 @@
 var ConsoleOutput = function() {
     this.colors = {};
+    this.colorMap = {
+    'black':'\x1b[30m',
+    'red':'\x1b[31m',
+    'green':'\x1b[32m',
+    'yellow':'\x1b[33m',
+    'blue':'\x1b[34m',
+    'magneta':'\x1b[35m',
+    'cyan':'\x1b[36m',
+    'white':'\x1b[37m'
+    };
 };
 
 ConsoleOutput.prototype.write = function(message, category) {
     if (this.colors && this.colors[category]) {
-        console.log(this.colors[category] + message + '\x1b[0m');
+        var colorCode = this.colors[category];
+        if(this.colorMap[colorCode]){
+            colorCode = this.colorMap[colorCode];
+        }
+        console.log(colorCode + message + '\x1b[0m');
     }
     else {
         console.log(message);
